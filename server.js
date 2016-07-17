1e2181ca51ffbe612ff01b543cea4cef9c0c8916
Lance Brown <lance.brown42@gmail.com>
2016-07-17 16:19:13 +0000

    more changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
-  res.sendFile('index.html')
+  res.sendFile('public/index.html')
 });
 
 // Creating Server and Listening for Connections \\
