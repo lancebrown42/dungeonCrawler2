b0b750f13121ed7e1caa7910f619ad57e2567b54
Lance <lance.brown42@gmail.com>
2016-07-20 16:24:48 -0600

    got rid of misleading text

--- public/game/menu.js
+++ public/game/menu.js
@@ -3,7 +3,7 @@ var menuState = {
 		
 		var nameLabel = game.add.text(250,80, "Skelly Crawler",{font: '50px Arial', fill:'#ffffff'})
 		var startLabel = game.add.text(80,game.world.height-120,"Press Space to Start",{font: '25px Arial', fill:'#ffffff'})
-		var startLabel = game.add.text(80,game.world.height-80,"WASD to move, E to interact, Space to attack",{font: '25px Arial', fill:'#ffffff'})
+		var instructionLabel = game.add.text(80,game.world.height-80,"WASD to move, E to interact, Space to attack",{font: '25px Arial', fill:'#ffffff'})
 
 		var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
--- public/game/win.js
+++ public/game/win.js
@@ -12,6 +12,12 @@ var winState = {
 
 	},
 	start : function(){
+		// game.destroy()
+		// var game = new Phaser.Game(800,600, Phaser.AUTO, 'gameDiv')
+		// game.state.add("menu", menuState)
+		// game.state.add("play", playState)
+		// game.state.add("win", winState)
+
 		game.state.start("menu")
 	}
 }
